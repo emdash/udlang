773c4a3950099311143dcf5abc1e10752ede405f
emdash <dotsony@gmail.com>
2021-03-10 11:18:04 -0800

    Add a simple tool for testing the parser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use udlang::grammar;
+use udlang::ast::Builder;
+
+use std::io::{
+    BufReader,
+    BufRead,
+    Read,
+    stdin
+};
+
+fn dump_expr(text: &str) {
+    let builder = Builder::new();
+    println!("{:?}", grammar::ExprParser::new().parse(&builder, text));
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+	let mut reader = BufReader::new(stdin());
+	let mut line = String::new();
+	reader.read_line(&mut line);
+	dump_expr(&line);
+    }
 }
