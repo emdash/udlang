b01981755d6601ef13d4615e0da8bcb97b4dbdae
emdash <dotsony@gmail.com>
2021-03-18 18:37:11 -0700

    Allow driving the parser and compiler from the main executable

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use std::env;
+use std::fs;
+use std::io;
+
 use udlang::grammar;
-use udlang::ast::Builder;
+use udlang::ast::{self, Builder};
+use udlang::ir::{self, Compiler};
 
 use std::io::{
     BufReader,
@@ -7,16 +12,54 @@ use std::io::{
     stdin
 };
 
-fn dump_expr(text: &str) {
+
+// Parse the given path to an AST
+fn parse(path: &str) -> ast::Program {
     let builder = Builder::new();
-    println!("{:?}", grammar::ExprParser::new().parse(&builder, text));
+    let contents = fs::read_to_string(path).expect(&format!("Couldn't read file: {}", path));
+    grammar::ProgramParser::new().parse(&builder, &contents).expect("Parse error")
 }
 
-fn main() {
+
+// Compile the given path to IR
+fn compile(path: &str) {
+    let mut compiler = Compiler::new();
+    println!("{:?}", compiler.compile_program(parse(path)))
+}
+
+
+// Dump expressions in a REPL
+fn dump_expr() {
+    let builder = Builder::new();
     loop {
 	let mut reader = BufReader::new(stdin());
 	let mut line = String::new();
 	reader.read_line(&mut line).unwrap();
-	dump_expr(&line);
+	println!("{:?}", grammar::ExprParser::new().parse(&builder, &line));
     }
+}
+
+
+// Try to parse `path` and print the resulting AST.
+fn dump_ast(path: &str) {
+    println!("{:?}", parse(path));
+}
+
+
+// Try to compile `path` and print the resulting IR.
+fn dump_ir(path: &str) {
+    println!("{:?}", compile(path))
+}
+
+
+fn main() {
+    let args: Vec<String> = env::args().into_iter().collect();
+    let strargs: Vec<&str> = args.iter().map(|i| i.as_str()).collect();
+    
+    match strargs.as_slice() {
+	[_, ("--dump-expr")] => dump_expr(),
+	[_, ("--dump-ast"), path] => dump_ast(path),
+	[_, ("--compile"), path] => dump_ir(path),
+	_ => println!("Invalid args. Usage string TBD.")
+    };
 }
