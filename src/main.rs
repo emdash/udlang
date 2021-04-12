11d7e91b927edf92b2d7f7284099ceebc7f8a8b9
emdash <dotsony@gmail.com>
2021-04-11 21:35:05 -0700

    While loops have no place in this language

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn dump_ir(path: &str) {
 // Try to decode an ir::Value from a string
 fn decode(_input: std::io::Result<String>) -> Value {
     // XXX: really implement me
-    Value::Bool(false)
+    Value::Int(0)
 }
 
 
